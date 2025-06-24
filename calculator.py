14059972170c750d558a3daac0a45e88714a87fa
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-06-24 05:25:54 +0200

    make the calculator a loop so user don't need to click run after every expression

--- calculator.py
+++ calculator.py
@@ -1,41 +1,48 @@
-
-
 def get_number(number):
-    while True:
-        try:
-            num1 = input(f'Number{number}: ')
-            num1 = float(num1)
-            return num1
-        except:
-            print('Invalid number ')
+        while True:
+            try:
+                num1 = input(f'Number{number}: ')
+                num1 = float(num1)
+                return num1
+            except:
+                print('Invalid number ')
 
 
-num1 = get_number(1)
+
+while True:
+
+    num1 = get_number(1)
 
 
-sign = input('Sign: ')
+    sign = input('Sign: ')
 
-num2 = get_number(2)
+    num2 = get_number(2)
 
 
-result = None
-if sign == '+':
-    result = num1 + num2
-elif sign == '-':
-    result = num1 - num2 
-elif sign == '/':
-    if num2 != 0:
-        result = num1 / num2 
+    result = None
+    if sign == '+':
+        result = num1 + num2
+    elif sign == '-':
+        result = num1 - num2 
+    elif sign == '/':
+        if num2 != 0:
+            result = num1 / num2 
+        else:
+            print('Error: Division by zero ')
+    elif sign == '*':
+        result = num1 * num2 
+    elif sign == '**':
+        result = num1 ** num2 
+    elif sign == '%':
+        result = num1 % num2 
     else:
-        print('Error: Division by zero ')
-elif sign == '*':
-    result = num1 * num2 
-elif sign == '**':
-    result = num1 ** num2 
-elif sign == '%':
-    result = num1 % num2 
- else:
-    print('Invalid operator! ')
+        print('Invalid operator! ')
 
 
-print(result)
+    print(result)
+
+    print('Another calculation? (yes/no)')
+    again = input('> ').lower()
+    if again != 'yes':
+        break
+    
